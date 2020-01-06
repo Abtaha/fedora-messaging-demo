7d92352a096504ae9b3c110c0cf507916dfc5372
Abtaha <abtaha2005@example.com>
2020-01-06 19:59:09 +0500

    Finalized

---
+++ publisher.py
@@ -0,0 +1,9 @@
+from fedora_messaging.api import publish, Message
+from fedora_messaging.config import conf
+
+conf.setup_logging()
+message = Message(
+    topic="tutorial.topic",
+    body={"reason": "test message"}
+)
+publish(message)
